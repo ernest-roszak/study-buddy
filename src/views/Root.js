7299fdee8d8a13e5baf82e4162d23211c5fba874
Ernest Roszak <ernest.roszak89@gmail.com>
2021-02-21 17:06:32 +0100

    added Theme Provider, Global Style  imports

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import UserList from '../components/UsersList/UsersList';
+import UserList from '../components/organisms/UsersList/UsersList';
+import styled, { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/globalStyles';
+import { theme } from 'assets/styles/theme';
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.lightGrey};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
 
 const Root = () => {
   return (
-    <>
-      <UserList />
-    </>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Wrapper>
+        <UserList />
+      </Wrapper>
+    </ThemeProvider>
   );
 };
 
