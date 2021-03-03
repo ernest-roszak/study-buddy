3e4bcd81c238fe48fc62ba4104f62d363fa1b84f
Ernest Roszak <ernest.roszak89@gmail.com>
2021-03-03 13:58:55 +0100

    added event delete usrer && fake API loading useEffect

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { users } from 'data/users';
+import React, { useEffect, useState } from 'react';
+import { users as usersData } from 'data/users';
 import UserListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList } from './UsersList.styles';
 
 const UserList = () => {
+  const [user, setAddUser] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+
+  const deleteUser = (name) => {
+    const filteredUsers = user.filter((user) => user.name !== name);
+    setAddUser(filteredUsers);
+    console.log('user', user);
+  };
+
+  const mockAPI = (success) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (usersData) {
+          resolve([...usersData]);
+        } else {
+          reject({ message: 'error' });
+        }
+      }, 2000);
+    });
+  };
+  useEffect(() => {
+    setIsLoading(true);
+    mockAPI()
+      .then((response) => {
+        setIsLoading(false);
+        setAddUser(response);
+      })
+      .catch((err) => console.log('err', err));
+  }, []);
+
   return (
     <Wrapper>
+      <h1>{isLoading && 'Loading...'}</h1>
       <StyledList>
-        {users.map((userData) => (
-          <UserListItem userData={userData} />
+        {user.map((userData) => (
+          <UserListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
         ))}
       </StyledList>
     </Wrapper>
