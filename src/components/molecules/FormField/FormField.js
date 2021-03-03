3451b9052f22505d4f1727a192608d0f609ee1dc
Ernest Roszak <ernest.roszak89@gmail.com>
2021-03-03 20:29:25 +0100

    Form field fucoinality added

--- src/components/molecules/FormField/FormField.js
+++ src/components/molecules/FormField/FormField.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Label } from 'components/atoms/Label/Label';
+import { Input } from 'components/atoms/Input/Input';
 
-const FormField = ({ name, id, type, ...props }) => {
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  ${Label} {
+    margin: 10px 0;
+  }
+`;
+
+const FormField = ({ onChange, value, name, id, label, type = 'string', ...props }) => {
   return (
-    <div>
-      <p>hello world</p>
-    </div>
+    <Wrapper>
+      <Label htmlFor={id}>{label}</Label>
+      <Input type={type} id={id} name={name} onChange={onChange} value={value} />
+    </Wrapper>
   );
 };
 
-FormField.propTypes = {};
+FormField.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
 
 export default FormField;
