b68cea38fda3a021c2d5a2cdc1b6415538738e15
Ernest Roszak <ernest.roszak89@gmail.com>
2021-03-03 14:18:12 +0100

    created new form field component

---
+++ src/components/molecules/FormField/FormField.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const FormField = ({ name, id, type, ...props }) => {
+  return (
+    <div>
+      <p>hello world</p>
+    </div>
+  );
+};
+
+FormField.propTypes = {};
+
+export default FormField;
