365d01f0c330b50eaa0c157b4ba08f4a881756bc
Ernest Roszak <ernest.roszak89@gmail.com>
2021-03-03 21:46:59 +0100

    created react route by router

--- src/components/organisms/UsersList/UsersList.js
+++ src/components/organisms/UsersList/UsersList.js
@@ -1,80 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { users as usersData } from 'data/users';
+import React from 'react';
 import UserListItem from 'components/molecules/UsersListItem/UsersListItem';
 import { Wrapper, StyledList, StyledTitle } from './UsersList.styles';
-import FormField from 'components/molecules/FormField/FormField';
-import { Button } from 'components/atoms/Button/Button';
 
-const mockAPI = (success) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (usersData) {
-        resolve([...usersData]);
-      } else {
-        reject({ message: 'error' });
-      }
-    }, 2000);
-  });
-};
-
-const initalState = {
-  name: '',
-  attendance: '',
-  average: '',
-};
-
-const UserList = () => {
-  const [user, setAddUser] = useState([]);
-  // const [usersData, setUsersData] = useState([]);
-  const [isLoading, setIsLoading] = useState([]);
-  const [formValue, setFormValue] = useState(initalState);
-
-  const deleteUser = (name) => {
-    const filteredUsers = user.filter((user) => user.name !== name);
-    setAddUser(filteredUsers);
-  };
-
-  const handleAddUser = (e) => {
-    e.preventDefault();
-    setAddUser([formValue, ...user]);
-    setFormValue(initalState);
-  };
-
-  const handleValueChange = (e) => {
-    setFormValue({ ...formValue, [e.target.name]: e.target.value });
-  };
-
-  useEffect(() => {
-    setIsLoading(true);
-    mockAPI()
-      .then((response) => {
-        setIsLoading(false);
-        setAddUser(response);
-      })
-      .catch((err) => console.log('err', err));
-  }, []);
-
+const UserList = ({ deleteUser, isLoading, user }) => {
   return (
-    <>
-      <Wrapper as="form">
-        <StyledTitle>Add new student</StyledTitle>
-        <FormField label="Name" id="name" name="name" onChange={handleValueChange} value={formValue.name} />
-        <FormField label="Attendance" id="attendance" name="attendance" onChange={handleValueChange} value={formValue.attendance} />
-        <FormField label="Average" id="average" name="average" onChange={handleValueChange} value={formValue.average} />
-        <Button type="submit" onClick={handleAddUser}>
-          Add
-        </Button>
-      </Wrapper>
-      <Wrapper>
-        <StyledTitle>Students Lists</StyledTitle>
-        <StyledTitle>{isLoading && 'Loading...'}</StyledTitle>
-        <StyledList>
-          {user.map((userData) => (
-            <UserListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
-          ))}
-        </StyledList>
-      </Wrapper>
-    </>
+    <Wrapper>
+      <StyledTitle>Students Lists</StyledTitle>
+      <StyledTitle>{isLoading && 'Loading...'}</StyledTitle>
+      <StyledList>
+        {user.map((userData) => (
+          <UserListItem deleteUser={deleteUser} key={userData.name} userData={userData} />
+        ))}
+      </StyledList>
+    </Wrapper>
   );
 };
 
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserList from '../components/organisms/UsersList/UsersList';
 import styled, { ThemeProvider } from 'styled-components';
+import { users as usersData } from 'data/users';
 import { GlobalStyle } from 'assets/styles/globalStyles';
 import { theme } from 'assets/styles/theme';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Form } from 'components/organisms/Form/Form';
 
 const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.lightGrey};
@@ -13,14 +16,73 @@ const Wrapper = styled.div`
   height: 100vh;
 `;
 
+const mockAPI = (success) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (usersData) {
+        resolve([...usersData]);
+      } else {
+        reject({ message: 'error' });
+      }
+    }, 2000);
+  });
+};
+
+const initalState = {
+  name: '',
+  attendance: '',
+  average: '',
+};
+
 const Root = () => {
+  const [user, setAddUser] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+  const [formValue, setFormValue] = useState(initalState);
+
+  const deleteUser = (name) => {
+    const filteredUsers = user.filter((user) => user.name !== name);
+    setAddUser(filteredUsers);
+  };
+
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    setAddUser([formValue, ...user]);
+    setFormValue(initalState);
+  };
+
+  const handleValueChange = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    mockAPI()
+      .then((response) => {
+        setIsLoading(false);
+        setAddUser(response);
+      })
+      .catch((err) => console.log('err', err));
+  }, []);
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Wrapper>
-        <UserList />
-      </Wrapper>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Wrapper>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/add-user">Add user</Link>
+          </nav>
+          <Switch>
+            <Route path="/add-user">
+              <Form handleAddUser={handleAddUser} formValue={formValue} handleValueChange={handleValueChange} />
+            </Route>
+            <Route path="/">
+              <UserList deleteUser={deleteUser} user={user} isLoading={isLoading} />
+            </Route>
+          </Switch>
+        </Wrapper>
+      </ThemeProvider>
+    </Router>
   );
 };
 
