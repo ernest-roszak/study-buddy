205d61b7ff653c5547c2f2205edb39c1d04bf8a8
Ernest Roszak <ernest.roszak89@gmail.com>
2021-03-29 15:33:15 +0200

    added grid to project

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,12 +1,16 @@
 import { Navigation } from 'components/organisms/Navigation/Navigation';
+import { SearchBar } from 'components/organisms/SearchBar/SearchBar';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
 import React from 'react';
+import { NewsSection } from '../NewsSection/NewsSection';
 
 export const MainTemplate = ({ children }) => {
   return (
     <Wrapper>
       <Navigation />
+      <SearchBar />
       {children}
+      <NewsSection />
     </Wrapper>
   );
 };
