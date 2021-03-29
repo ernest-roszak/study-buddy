9392d26e0bca82b27960622fa055a09cf3943d6c
Ernest Roszak <ernest.roszak89@gmail.com>
2021-03-29 17:15:18 +0200

    fixed tests

--- src/views/AddUser.test.js
+++ src/views/AddUser.test.js
@@ -5,7 +5,22 @@ import AddUser from './AddUser';
 import Dashboard from './Dashboard';
 
 describe('Add user', () => {
-  it('Renders components', () => {
+  it('adds new user to the list', () => {
+    renderWithProviders(
+      <>
+        <AddUser />
+        <Dashboard />
+      </>
+    );
+    fireEvent.change(screen.getByTestId('Name'), { target: { value: 'Ernest' } });
+    fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
+    fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
+    fireEvent.click(screen.getByTestId('Consent'));
+    fireEvent.click(screen.getByText('Add'));
+    screen.getByText('Ernest');
+  });
+
+  it('prevent adding new user if the consent is not checked', () => {
     renderWithProviders(
       <>
         <AddUser />
@@ -16,6 +31,7 @@ describe('Add user', () => {
     fireEvent.change(screen.getByTestId('Attendance'), { target: { value: '55%' } });
     fireEvent.change(screen.getByTestId('Average'), { target: { value: '4.5' } });
     fireEvent.click(screen.getByText('Add'));
-    screen.getByText('Ernest');
+    const newUser = screen.queryByText('Ernest');
+    expect(newUser).not.toBeInTheDocument();
   });
 });
