a18794c0b8713461530011bb8e02cb093447cebe
Ernest Roszak <ernest.roszak89@gmail.com>
2021-03-29 17:19:58 +0200

    refactoring

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -2,7 +2,7 @@ import { Navigation } from 'components/organisms/Navigation/Navigation';
 import { SearchBar } from 'components/organisms/SearchBar/SearchBar';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
 import React from 'react';
-import { NewsSection } from '../NewsSection/NewsSection';
+import NewsSection from '../NewsSection/NewsSection';
 
 export const MainTemplate = ({ children }) => {
   return (
