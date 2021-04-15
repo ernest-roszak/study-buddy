9d299fb0b797d6a9085ca788f0e84f8c8e3495e4
Ernest Roszak <ernest.roszak89@gmail.com>
2021-04-15 14:58:35 +0200

    refactoring

--- src/views/Root.js
+++ src/views/Root.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/globalStyles';
 import { theme } from 'assets/styles/theme';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { MainTemplate } from 'components/templates/MainTemplate/MainTemplate';
 import { Wrapper } from './Root.styles';
 import AddUser from 'views/AddUser';
 import Dashboard from 'views/Dashboard';
-import { UsersProvider } from 'providers/UsersProviders';
 
 const Root = () => {
   return (
@@ -15,18 +14,19 @@ const Root = () => {
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <MainTemplate>
-          <UsersProvider>
-            <Wrapper>
-              <Switch>
-                <Route path="/add-user">
-                  <AddUser />
-                </Route>
-                <Route path="/">
-                  <Dashboard />
-                </Route>
-              </Switch>
-            </Wrapper>
-          </UsersProvider>
+          <Wrapper>
+            <Switch>
+              <Route exact path="/">
+                <Redirect to="/group" />
+              </Route>
+              {/* <Route path="/add-user">
+                <AddUser />
+              </Route> */}
+              <Route path="/group/:id?">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </Wrapper>
         </MainTemplate>
       </ThemeProvider>
     </Router>
