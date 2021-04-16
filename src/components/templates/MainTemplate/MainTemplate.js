2a68a44334d6d169697cfb84e0eab24381b37f0b
Ernest Roszak <ernest.roszak89@gmail.com>
2021-04-16 14:51:37 +0200

    reafctoring

--- src/components/templates/MainTemplate/MainTemplate.js
+++ src/components/templates/MainTemplate/MainTemplate.js
@@ -1,7 +1,7 @@
 import { Navigation } from 'components/organisms/Navigation/Navigation';
 import { SearchBar } from 'components/organisms/SearchBar/SearchBar';
 import { Wrapper } from 'components/templates/MainTemplate/MainTemplate.styles';
-import React, { useState } from 'react';
+import React from 'react';
 import NewsSection from '../NewsSection/NewsSection';
 
 export const MainTemplate = ({ children }) => {
