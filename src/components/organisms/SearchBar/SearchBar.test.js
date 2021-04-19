b73aebc7bcffed3cfb56d949120bd5834f47c05c
Ernest Roszak <ernest.roszak89@gmail.com>
2021-04-19 11:34:25 +0200

    tests added

--- src/components/organisms/SearchBar/SearchBar.test.js
+++ src/components/organisms/SearchBar/SearchBar.test.js
@@ -24,13 +24,25 @@ describe('Search Bar', () => {
     await screen.findByText(/Adam Ro/);
   });
 
-  it('Check that the list witch users is open', async () => {
+  it('Hides the results when input is empty', async () => {
     render(<SearchBar />);
     const input = screen.getByPlaceholderText('Search');
     fireEvent.change(input, { target: { value: 'ad' } });
     await screen.findByText(/Adam Ro/);
 
     fireEvent.change(input, { target: { value: '' } });
+    await waitFor(() => {
+      expect(screen.getByLabelText('results')).not.toBeVisible();
+    });
+  });
+
+  it('Hides the results when button esc is pressed', async () => {
+    render(<SearchBar />);
+    const input = screen.getByPlaceholderText('Search');
+    fireEvent.change(input, { target: { value: 'ad' } });
+    await screen.findByText(/Adam Ro/);
+
+    fireEvent.keyDown(input, { key: 'Esc', code: 'Esc' });
     await waitFor(() => {
       expect(screen.getByLabelText('results')).not.toBeVisible();
     });
