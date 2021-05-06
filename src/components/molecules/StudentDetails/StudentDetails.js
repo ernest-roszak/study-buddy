ca1663350ab057d63263337cdf726b2504925203
Ernest Roszak <ernest.roszak89@gmail.com>
2021-05-06 11:03:18 +0200

    Styled modal with StudentDetails

--- src/components/molecules/StudentDetails/StudentDetails.js
+++ src/components/molecules/StudentDetails/StudentDetails.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Title } from 'components/atoms/Title/Title';
 import { Average } from 'components/atoms/Average/Average';
+import { BigAverage, StyledDetails, StyledInfo, StyledLabel, StyledSubjectInfo, Wrapper } from './StudentDetails.styles';
 
 const StudentDetails = ({ student }) => {
   return (
-    <div>
-      <Title>
-        {student.name} | Group: {student.group}
-      </Title>
-      <p> {student.attendance}</p>
-      <Average value={student.average}>{student.average}</Average>
-    </div>
+    <Wrapper>
+      <BigAverage value={student.average}>{student.average}</BigAverage>
+      <Title isBig>{student.name}</Title>
+      <StyledDetails>
+        <StyledLabel>Course:</StyledLabel>
+        <StyledInfo isBig>{student.course}</StyledInfo>
+        <StyledLabel>Average grades:</StyledLabel>
+        {student.grades.map(({ subject, average }) => (
+          <StyledSubjectInfo key={subject}>
+            <StyledInfo>{subject}</StyledInfo>
+            <Average value={average}>{average}</Average>
+          </StyledSubjectInfo>
+        ))}
+      </StyledDetails>
+    </Wrapper>
   );
 };
 
