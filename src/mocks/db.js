9c541681c21617aa6280ecc00eb326eb1ff4633c
Ernest Roszak <ernest.roszak89@gmail.com>
2021-05-06 14:16:31 +0200

    created model student for database

---
+++ src/mocks/db.js
@@ -0,0 +1,35 @@
+// src/mocks/db.js
+import { factory, primaryKey } from '@mswjs/data';
+import faker from 'faker';
+
+faker.seed(123);
+
+const groups = ['A', 'B', 'C'];
+const getRandomAverage = () => `${faker.datatype.number({ min: 0, max: 5, precision: 0.1 })}`;
+const getRandomGroup = (index) => groups[index];
+
+export const db = factory({
+  // Create a "user" model,
+  student: {
+    id: primaryKey(faker.datatype.uuid),
+    name: () => faker.fake('{{name.firstName}} {{name.lastName,}}'),
+    attendance: () => `${faker.datatype.number({ min: 0, max: 100 })}`,
+    average: getRandomAverage,
+    group: getRandomGroup(faker.datatype.number({ min: 0, max: 2 })),
+    course: () => faker.fake('{{random.word}} {{random.word}}'),
+    grades: [
+      {
+        subject: 'Business Philosophy',
+        average: getRandomAverage(),
+      },
+      {
+        subject: 'Marketing',
+        average: getRandomAverage(),
+      },
+      {
+        subject: 'Modern Economy',
+        average: getRandomAverage(),
+      },
+    ],
+  },
+});
