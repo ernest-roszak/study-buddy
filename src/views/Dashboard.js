269e8db5d22931ebaa84bf2b780c57be490cef62
Ernest Roszak <ernest.roszak89@gmail.com>
2021-05-15 13:38:27 +0200

    refactor in mocks folders fixed bug

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,24 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import StudentsList from 'components/organisms/StudentsList/StudentsList';
-import { Link, Redirect, useParams } from 'react-router-dom';
-import { Title } from 'components/atoms/Title/Title';
-import { GroupWrapper, TitleWrapper, Wrapper } from './Dashboard.styles';
 import { useStudents } from 'hooks/useStudents';
+import { GroupWrapper, TitleWrapper, Wrapper } from 'views/Dashboard.styles';
+import { Title } from 'components/atoms/Title/Title';
 import useModal from 'components/organisms/Modal/useModal';
-
 import StudentDetails from 'components/molecules/StudentDetails/StudentDetails';
 import Modal from 'components/organisms/Modal/Modal';
 
 const Dashboard = () => {
   const [groups, setGroups] = useState([]);
-  const [currentStudent, setCurrentStudent] = useState([]);
+  const [currentStudent, setCurrentStudent] = useState(null);
   const { getGroups, getStudentById } = useStudents();
   const { id } = useParams();
-  const { isOpen, handleCloseModal, handleOpenModal } = useModal();
+  const { isOpen, handleOpenModal, handleCloseModal } = useModal();
 
   useEffect(() => {
     (async () => {
       const groups = await getGroups();
+      console.log(groups);
       setGroups(groups);
     })();
   }, [getGroups]);
@@ -29,23 +30,23 @@ const Dashboard = () => {
     handleOpenModal();
   };
 
-  if (!id && groups.length > 0) return <Redirect to={`/group/${groups[0]}`} />;
+  if (!id && groups.length > 0) return <Redirect to={`/group/${groups[0].id}`} />;
 
   return (
     <Wrapper>
       <TitleWrapper>
         <Title as="h2">Group {id}</Title>
         <nav>
-          {groups.map((group) => (
-            <Link key={group} to={`/group/${group}`}>
-              {group}{' '}
+          {groups.map(({ id }) => (
+            <Link key={id} to={`/group/${id}`}>
+              {id}{' '}
             </Link>
           ))}
         </nav>
       </TitleWrapper>
       <GroupWrapper>
         <StudentsList handleOpenStudentDetails={handleOpenStudentDetails} />
-        <Modal handleClose={handleCloseModal} isOpen={isOpen}>
+        <Modal isOpen={isOpen} handleClose={handleCloseModal}>
           <StudentDetails student={currentStudent} />
         </Modal>
       </GroupWrapper>
