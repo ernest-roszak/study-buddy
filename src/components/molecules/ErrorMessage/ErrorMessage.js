b1b700ba1ea81cac2fe37c13dbe101b3e58e0169
Ernest Roszak <ernest.roszak89@gmail.com>
2021-06-13 19:23:09 +0200

    cteated ErrorMessage component

---
+++ src/components/molecules/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Wrapper } from './ErrorMessage.styles';
+import { Title } from 'components/atoms/Title/Title';
+
+const defaultMessageError = 'Something went wrong. Please try again, or contact our support.';
+
+const ErrorMessage = ({ message = defaultMessageError }) => {
+  return (
+    <Wrapper>
+      <Title>Opps!</Title>
+      <p>{message}</p>
+    </Wrapper>
+  );
+};
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string,
+};
+
+export default ErrorMessage;
