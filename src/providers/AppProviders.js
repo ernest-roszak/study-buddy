460d009993e4bc852350001b938d8de30f7727b9
Ernest Roszak <ernest.roszak89@gmail.com>
2021-06-13 19:24:20 +0200

    created AppProviders component

---
+++ src/providers/AppProviders.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'assets/styles/globalStyles';
+import { theme } from 'assets/styles/theme';
+import { AuthProvider } from 'hooks/useAuth';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+const AppProviders = ({ children }) => {
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <GlobalStyle />
+          {children}
+        </AuthProvider>
+      </ThemeProvider>
+    </Router>
+  );
+};
+
+export default AppProviders;
