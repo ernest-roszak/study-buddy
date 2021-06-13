3870fbc391b950b2cd38dc31336c05fc95a8f8c1
Ernest Roszak <ernest.roszak89@gmail.com>
2021-06-13 20:26:23 +0200

    added ErrorProvider

--- src/providers/AppProviders.js
+++ src/providers/AppProviders.js
@@ -3,16 +3,19 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/globalStyles';
 import { theme } from 'assets/styles/theme';
 import { AuthProvider } from 'hooks/useAuth';
+import { ErrorProvider } from 'hooks/useError';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 const AppProviders = ({ children }) => {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <GlobalStyle />
-          {children}
-        </AuthProvider>
+        <ErrorProvider>
+          <AuthProvider>
+            <GlobalStyle />
+            {children}
+          </AuthProvider>
+        </ErrorProvider>
       </ThemeProvider>
     </Router>
   );
