fbb2feaa40e085a3866fbd29f6c5b6d8eaeb37dd
Ernest Roszak <ernest.roszak89@gmail.com>
2021-06-13 19:24:38 +0200

    fixed type problem

--- src/types/index.js
+++ src/types/index.js
@@ -3,6 +3,6 @@ import PropTypes from 'prop-types';
 export const UserShape = {
   name: PropTypes.string,
   attendance: PropTypes.string,
-  average: PropTypes.string,
+  average: PropTypes.number,
   group: PropTypes.string,
 };
